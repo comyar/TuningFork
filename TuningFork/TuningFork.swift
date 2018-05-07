dfccd9127db9dad66681f8959b0c4d91d59736a5
bdutton <brian@followapp.me>
2018-05-07 10:40:22 -0400

    added missing try

--- TuningFork/TuningFork.swift
+++ TuningFork/TuningFork.swift
@@ -151,7 +151,7 @@ A Tuner uses the devices microphone and interprets the frequency, pitch, etc.
     microphone.start()
     tracker.start()
     AudioKit.output = silence 
-    AudioKit.start()
+    try? AudioKit.start()
     
     if timer == nil {
       timer = DispatchTimer(interval: 0.03, closure: { (t, i) -> Void in
@@ -177,7 +177,7 @@ A Tuner uses the devices microphone and interprets the frequency, pitch, etc.
   public func stop() {
     microphone.stop()
     tracker.stop()
-    AudioKit.stop()
+    try? AudioKit.stop()
     timer?.pause()
   }
   
